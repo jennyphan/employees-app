f712e36b2a9adadad7f1e5a72ad2b46c44d400df
jenny phan <spinningjphan@gmail.com>
2020-04-08 19:05:44 -0500

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'view-employee/:id',
+    loadChildren: () =>
+      import('./employees/employee-detail/employee-detail.module').then(
+        (m) => m.EmployeeDetailModule
+      ),
+  },
+  {
+    path: 'employees',
+    loadChildren: () =>
+      import('./employees/employee-list/employee-list.module').then(
+        (m) => m.EmployeeListModule
+      ),
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/employees/employee-list/employee-list-routing.module.ts
+++ src/app/employees/employee-list/employee-list-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { EmployeeListComponent } from './employee-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: EmployeeListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class EmployeeListRoutingModule { }
+export class EmployeeListRoutingModule {}

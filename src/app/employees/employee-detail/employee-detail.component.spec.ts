79a6200973685cfbadb689b18946101f9558c888
jenny phan <spinningjphan@gmail.com>
2020-04-09 10:15:49 -0500

    Fix unit tests

--- src/app/employees/employee-detail/employee-detail.component.spec.ts
+++ src/app/employees/employee-detail/employee-detail.component.spec.ts
@@ -1,16 +1,42 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable, of } from 'rxjs';
+import { EmployeeApiService } from '../../core/services/employee-api.service';
+import { EmployeeResponse } from '../../shared/models/employee-response';
 import { EmployeeDetailComponent } from './employee-detail.component';
 
+const EMPLOYEE_RESPONSE_OBJECT: EmployeeResponse = {
+  status: 'success',
+  employees: {
+    id: '1',
+    name: 'testName',
+    salary: '50,000',
+    age: '22',
+    profileImage: '',
+  },
+};
+class MockEmployeeService {
+  employeeApiUrl = 'http://dummy.restapiexample.com/api/v1';
+  public getEmployee(id: string): Observable<EmployeeResponse> {
+    return of(EMPLOYEE_RESPONSE_OBJECT);
+  }
+}
 describe('EmployeeDetailComponent', () => {
   let component: EmployeeDetailComponent;
   let fixture: ComponentFixture<EmployeeDetailComponent>;
 
+  let employeeService: EmployeeApiService;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EmployeeDetailComponent ]
-    })
-    .compileComponents();
+      declarations: [EmployeeDetailComponent],
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      providers: [
+        EmployeeDetailComponent,
+        { provide: EmployeeApiService, useClass: MockEmployeeService },
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
--- src/app/employees/employee-list/employee-list.component.spec.ts
+++ src/app/employees/employee-list/employee-list.component.spec.ts
@@ -1,6 +1,36 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NgxPaginationModule } from 'ngx-pagination';
+import { Observable, of } from 'rxjs';
+import { EmployeeApiService } from '../../core/services/employee-api.service';
+import { EmployeeResponse } from '../../shared/models/employee-response';
 import { EmployeeListComponent } from './employee-list.component';
+
+const EMPLOYEES_RESPONSE_OBJECT: EmployeeResponse = {
+  status: 'success',
+  employees: [
+    {
+      id: '1',
+      name: 'testName',
+      salary: '50,000',
+      age: '22',
+      profileImage: '',
+    },
+    {
+      id: '2',
+      name: 'testName2',
+      salary: '650,000',
+      age: '29',
+      profileImage: '',
+    },
+  ],
+};
+class MockEmployeeService {
+  employeeApiUrl = 'http://dummy.restapiexample.com/api/v1';
+  public getEmployees(): Observable<EmployeeResponse> {
+    return of(EMPLOYEES_RESPONSE_OBJECT);
+  }
+}
 
 describe('EmployeeListComponent', () => {
   let component: EmployeeListComponent;
@@ -8,9 +38,13 @@ describe('EmployeeListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EmployeeListComponent ]
-    })
-    .compileComponents();
+      declarations: [EmployeeListComponent],
+      imports: [HttpClientTestingModule, NgxPaginationModule],
+      providers: [
+        EmployeeListComponent,
+        { provide: EmployeeApiService, useClass: MockEmployeeService },
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
